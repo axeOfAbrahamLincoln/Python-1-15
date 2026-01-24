f002cfee346eaa3f1050fbc501ca9946c105f29f
axeOfAbrahamLincoln <kormociadam@gmail.com>
2026-01-24 23:42:49 +0100

    Initializes coffee machine structure
    Sets up the basic data structure for the coffee machine, including the tank and coin slot.

---
+++ day 15/caffe_maschine.py
@@ -0,0 +1,18 @@
+# TODO1: ask user what he would like to drink
+# TODO2: 
+
+from data import MENU
+
+caffe_machine = {
+    'tank' : {
+        'water' : 0,
+        'milk'  : 0,
+        'coffee': 0,
+    },
+    'coin slot' : 0,
+}
+
+
+
+
+    
